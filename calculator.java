91d3535ef7943bec18b04172665849d481871be6
Anupamajha2 <anupamajha11233@gmail.com>
2023-04-06 14:49:00 +0530

    Add calculator code

---
+++ calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+class Calculator{
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+       
+        char operator;
+        double num1,num2,result;
+         //Asking what operation needs to be done 
+         System.out.println("Choose an operator : +,-,*,/");
+         operator= input.next().charAt(0);
+
+         // taking inputs of the two numbers
+         System.out.println("Enter number1");
+         num1=input.nextDouble() ;
+         System.out.println("Enter number2");
+         num2=input.nextDouble();
+
+    switch (operator) {
+        case '+':
+
+        result = num1+ num2;
+        System.out.println("the sum of two numbers is " + result);
+            break;
+            case '-':
+
+        result = num1- num2;
+        System.out.println("the substraction of two numbers is " + result);
+            break;
+        case '*':
+        result = num1*num2;
+        System.out.println("the substraction of two numbers is " + result);
+            break;
+        case '/':
+         result = num1/ num2;
+        System.out.println("the divisiion of two numbers is " + result);
+            break;
+    
+        default:
+        System.out.println("Not valid");
+            break;
+    }
+
+    } }
